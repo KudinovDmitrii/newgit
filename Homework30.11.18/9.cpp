44dcf62a0139dac922465fc0064112bcaea5d694
KudinovDmitrii <44033451+KudinovDmitrii@users.noreply.github.com>
2018-11-25 20:27:29 +0300

    Homework30.11.2018

--- Homework30.11.18/9.cpp
+++ Homework30.11.18/9.cpp
@@ -6,13 +6,13 @@ int main()
 {
 	int n;
 	cin >> n;
-	bool *used = new bool[n + 1000000];
-	for (int i = 2; i <= n; i++) {
+	bool *used = new bool[2*n +1];
+	for (int i = 2; i <= 2 * n + 1; i++) {
 		used[i] = 1;
 	}
 	for (int k = 2; k <= n; k++) {
 		if (used[k] != 0) {
-			for (int j = k + k; j <= n + 1000000 - 1; j += k) {
+			for (int j = k + k; j <= 2 * n + 1 ; j += k) {
 				used[j] = 0;
 
 			}
@@ -21,7 +21,6 @@ int main()
 			}
 		}
 	}
-	
-		return 0;
-}
 
+	return 0;
+}
